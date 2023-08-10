78886a6894b06440f0ede41cc224573e8a1c317d
João Pedro <joaopedro.nc@outlook.com>
2023-08-10 13:53:34 -0300

    🔧 chore: Set static files

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ const path = require('path')
 
 const server = express()
 
+server.use(express.static("public"))
+
 server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, 'views'))
 
 server.use(routes)
-
 
 server.listen(3000, () => console.log('Server is Running'))
