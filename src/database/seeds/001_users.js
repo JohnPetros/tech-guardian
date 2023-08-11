b2a667a28cfbd93fcd4e2f7a5168e7c37da1f73a
João Pedro <joaopedro.nc@outlook.com>
2023-08-11 15:08:27 -0300

    🔧 chore: Add user data seed

---
+++ src/database/seeds/001_users.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('users').del()
+  await knex('users').insert([
+    {
+      id: 1,
+      name: 'John Petros',
+      email: 'johnpetros@gmail.com',
+      password: '123',
+    },
+  ])
+}
