61c267561a713b7755d173c186b764fe7240b288
João Pedro <joaopedro.nc@outlook.com>
2023-08-12 15:47:17 -0300

    🔒 Add server error handler

--- src/middlewares/handleServerError.js
+++ src/middlewares/handleServerError.js
@@ -1,15 +1,5 @@
-const ServerError = require('../errors/ServerError')
-
-function handleServerError(error, request, response, next) {
-  if (error instanceof ServerError) {
-    return response.status(error.status).render(error.page, {
-      message: {
-        type: 'error',
-        body: error.message,
-      },
-      ...error.pageData,
-    })
-  }
+function handleServerError(error, request, response) {
+  console.error(error);
 
   response.status(500).render('pages/error.ejs')
 }
