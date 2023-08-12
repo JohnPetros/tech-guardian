a531e2ce9b0f5c3633f445d9b7cb96ea91c811f0
João Pedro <joaopedro.nc@outlook.com>
2023-08-12 18:29:04 -0300

    🔧 chore: Add database env to knex config

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const environment = process.env.ENVIRONMENT;
 const knexConfig = require("../../knexfile");
-const knex = require("knex")(knexConfig.development)
+const knex = require("knex")(knexConfig[environment])
 
 module.exports = knex
