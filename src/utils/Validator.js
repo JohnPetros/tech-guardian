3b28d9605403b8697d0ae6b4ee1e508385600765
João Pedro <joaopedro.nc@outlook.com>
2023-08-12 20:55:24 -0300

    🔒 Add validateRegister validation

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -11,6 +11,22 @@ const loginValidation = yup.object().shape({
     ),
 })
 
+const registerValidation = yup.object().shape({
+  name: yup.string().required().min(3, 'Nome de usuário deve ter pelo menos 3 caracteres'),
+  email: yup.string().required('Por favor, insira um e-mail').email(),
+  password: yup
+    .string()
+    .required('Por favor, insira uma senha')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g,
+      'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial.'
+    ),
+  passwordConfirmation: yup
+    .string()
+    .required('Por favor, confirme sua senha')
+    .oneOf([yup.ref('password')], 'Senhas não conferem'),
+})
+
 class Validator {
   async validate(validation) {
     try {
@@ -23,6 +39,16 @@ class Validator {
   async validateLogin(email, password) {
     return this.validate(() =>
       loginValidation.validate({ email, password }, { abortEarly: false })
+    )
+  }
+
+  async validateRegister({ name, email, password, passwordConfirmation }) {
+    console.log(passwordConfirmation);
+    return this.validate(() =>
+      registerValidation.validate(
+        { name, email, password, passwordConfirmation },
+        { abortEarly: false }
+      )
     )
   }
 }
