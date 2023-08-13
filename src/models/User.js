b652acae5357d8673ee5d9d618831dd723ff3bd6
João Pedro <joaopedro.nc@outlook.com>
2023-08-13 16:23:08 -0300

    🔒 Add try catch to User methods

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,28 @@
 const knex = require('../database')
 
 class User {
+  async execute(method) {
+    try {
+      return await method()
+    } catch (error) {
+      throw new ServerError(error)
+    }
+  }
+
   async create({ id, name, email, password }) {
-    return knex('users')
-      .returning(['id', 'name', 'email', 'avatar'])
-      .insert({ id, name, email, password })
+    const createdUser = await this.execute(() =>
+      knex('users')
+        .returning(['id', 'name', 'email', 'avatar'])
+        .insert({ id, name, email, password })
+    )
+
+    return createdUser
   }
 
   async findByEmail(email) {
-    const user = await knex('users').where({ email }).first()
+    const user = await this.execute(() =>
+      knex('users').where({ email }).first()
+    )
 
     return user
   }
