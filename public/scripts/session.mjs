97a65b0a11f73f7a4324aa378000c87f9a6826a7
João Pedro <joaopedro.nc@outlook.com>
2023-08-13 23:13:21 -0300

    ✏️ Add module extension to form and toat scripts

--- public/scripts/session.mjs
+++ public/scripts/session.mjs
@@ -1,8 +1,7 @@
-import { Form } from "./modules/form.js"
-import { Toast } from "./modules/toast.js"
+import { Form } from './modules/form.mjs'
+import { Toast } from './modules/toast.mjs'
 
 new Form('[data-form]')
 
 const toast = new Toast('[data-toast]')
 toast.open()
-
