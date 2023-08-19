41f505d2b6e9aac9a6e093ac7e0cee3879ce548a
João Pedro <joaopedro.nc@outlook.com>
2023-08-19 16:55:51 -0300

    🔒 Add checkSession middleware to Orders routes

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,12 +1,19 @@
 const { Router } = require('express')
 const OrdersController = require('../controllers/OrdersController')
+const checkSession = require('../middlewares/checkSession')
 
 const ordersRouter = new Router()
 
 const ordersController = new OrdersController()
 
+ordersRouter.use(checkSession)
+
 ordersRouter.get('/open-orders', ordersController.renderOpenOrdersPage)
 
-ordersRouter.get('/orders/:orderId', ordersController.renderOrderPage)
+ordersRouter.get('/new-order', ordersController.renderNewOrderPage)
+
+ordersRouter.get('/order/:orderId', ordersController.renderOrderPage)
+
+ordersRouter.get('/order/resolve', ordersController.renderOrderPage)
 
 module.exports = ordersRouter
