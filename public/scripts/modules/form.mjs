634c2f7d4dc1c967a436b716c22a21c9148718b0
João Pedro <joaopedro.nc@outlook.com>
2023-08-19 17:24:34 -0300

    ✨ feat: Auto adjust textarea height

--- public/scripts/modules/form.mjs
+++ public/scripts/modules/form.mjs
@@ -1,6 +1,9 @@
 export class Form {
+  validations = ['email', 'password', 'required', 'min-length', 'equal']
+
   emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g
+  passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g
   specialChars = {
     '&': '&amp;',
     '<': '&lt;',
@@ -12,36 +15,38 @@ export class Form {
 
   constructor(form) {
     this.form = document.querySelector(form)
-    this.inputs = this.form.querySelectorAll('input')
-    this.textareas = this.form.querySelectorAll('textarea')
+    this.inputs = this.form.querySelectorAll('[data-input]')
+    this.textareas = this.form.querySelectorAll('[data-textarea]')
     this.radios = this.form.querySelectorAll('[data-radio]')
+
+    this.onTextareaKeyup = this.onTextareaKeyup.bind(this)
+    this.onRadioKeyDown = this.onRadioKeyDown.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.form.addEventListener('submit', this.onSubmit)
 
     if (this.textareas.length) {
       this.textareas.forEach((textarea) => {
-        textarea.addEventListener('keyup', this.onKeyUp)
-        this.adjustSize(textarea)
+        textarea.addEventListener('input', this.onTextareaKeyup)
+        this.adjustTextareaSize(textarea)
       })
     }
 
     if (this.radios.length) {
       this.radios.forEach((radio) =>
-        radio.addEventListener('keydown', this.onKeyDown)
+        radio.addEventListener('keydown', this.onRadioKeyDown)
       )
     }
-
-    this.onKeyUp = this.onKeyUp.bind(this)
-    this.onKeyDown = this.onKeyDown.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.form.addEventListener('submit', this.onSubmit)
   }
 
-  adjustSize(textarea) {
-    textarea.style.height = ''
+  adjustTextareaSize(textarea) {
+    console.log(textarea)
+    textarea.style.height = 'auto'
     textarea.style.height = textarea.scrollHeight + 'px'
   }
 
-  onKeyUp(event) {
-    this.adjustSize(event.currentTarget)
+  onTextareaKeyup(event) {
+    console.log(this)
+    this.adjustTextareaSize(event.currentTarget)
   }
 
   getInput(inputId) {
@@ -130,6 +135,12 @@ export class Form {
     for (const [validationType, validationValue] of Object.entries(
       validations
     )) {
+      const isValidation = this.validations.includes(validationType)
+
+      if (!isValidation) {
+        continue
+      }
+
       this[validationType](input, validationValue)
     }
   }
@@ -140,7 +151,7 @@ export class Form {
       .replace(/[&<>"']/g, (match) => this.specialChars[match])
   }
 
-  onKeyDown(event) {
+  onRadioKeyDown(event) {
     if (event.key === 'Enter') {
       event.currentTarget.click()
     }
@@ -151,8 +162,10 @@ export class Form {
 
     this.removeErrors()
 
-    this.inputs.forEach(this.sanitizeInputs, this)
-    this.inputs.forEach(this.validateInput, this)
+    if (this.inputs.length) {
+      this.inputs.forEach(this.sanitizeInputs, this)
+      this.inputs.forEach(this.validateInput, this)
+    }
 
     const hasErrors = this.hasErrors()
 
