4c998cd2e086aacb972248aea753bfaba4236c80
João Pedro <joaopedro.nc@outlook.com>
2023-08-20 11:21:43 -0300

    ♿ Add flash messages to check role middleware

--- src/middlewares/checkRole.js
+++ src/middlewares/checkRole.js
@@ -1,12 +1,19 @@
 const getPreviousRoute = require('../helpers/getPreviousRoute')
+const FlashMessage = require('../utils/FlashMessage')
 
-function checkRole(role) {
+function checkRole(role, errorMessage) {
   return (request, response, next) => {
     const { user } = request.session
 
     const previousRoute = getPreviousRoute(request)
 
     if (user.role !== role) {
+      const flashMessage = new FlashMessage(response.flash)
+
+      flashMessage.add('error', errorMessage)
+
+      flashMessage.addMultipleByRoute(previousRoute, request.body)
+
       return response.redirect(previousRoute)
     }
 
