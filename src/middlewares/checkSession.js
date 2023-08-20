588fe02a7f5215f7051852a0d39474894fa7b806
João Pedro <joaopedro.nc@outlook.com>
2023-08-20 13:51:38 -0300

    🔒 Check session on every route

--- src/middlewares/checkSession.js
+++ src/middlewares/checkSession.js
@@ -4,10 +4,17 @@ function checkSession(request, response, next) {
   const { user } = request.session
 
   const currentRoute = request.originalUrl
+  const previousRoute = request.session.previousRoute ?? '/open-orders'
+
+  if (user && publicRoutes.includes(currentRoute)) {
+    return response.redirect(previousRoute)
+  }
 
   if (!user && !publicRoutes.includes(currentRoute)) {
     return response.redirect('/')
   }
+
+  request.session.previousRoute = currentRoute
 
   next()
 }
