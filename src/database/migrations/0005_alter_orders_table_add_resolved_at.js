05a16d294e8030a92e8c097f250f0e5710c843ae
João Pedro <joaopedro.nc@outlook.com>
2023-08-21 17:11:18 -0300

    🗄️ Add alter orders table add resolved_at column migration

---
+++ src/database/migrations/0005_alter_orders_table_add_resolved_at.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  return knex.schema.alterTable('orders', (table) => {
+    table.timestamp('resolved_at')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+  return knex.schema.table('orders', (table) => {
+    table.dropColumn('resolved_at')
+  })
+}
