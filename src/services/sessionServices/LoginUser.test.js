30be28c2678c925502fbad29374f294a6c67718c
João Pedro <joaopedro.nc@outlook.com>
2023-08-21 12:54:18 -0300

    🧪 Test Login user service

--- src/services/sessionServices/LoginUser.test.js
+++ src/services/sessionServices/LoginUser.test.js
@@ -1,23 +1,23 @@
-const MockRole = require('../../mocks/MockRole')
-const MockUser = require('../../mocks/mockUser')
-const LoginUser = require('./LoginUser')
+const RoleMock = require('../../mocks/RoleMock')
+const UserMock = require('../../mocks/UserMock')
+const LoginUserService = require('./LoginUserService')
 
 describe('Login user service', () => {
-  let mockRole = null
-  let mockUser = null
-  let loginUser = null
+  let roleMock = null
+  let userMock = null
+  let loginUserService = null
 
   beforeEach(() => {
-    mockRole = new MockRole()
-    mockUser = new MockUser()
-    loginUser = new LoginUser(mockUser, mockRole)
+    roleMock = new RoleMock()
+    userMock = new UserMock()
+    loginUserService = new LoginUserService(userMock, roleMock)
   })
 
   it('should login user when email and password are correct', async () => {
     const email = 'johndoe@mock.com'
     const password = 'jonhDOE77$'
 
-    const { errors, user } = await loginUser.execute(email, password)
+    const { errors, user } = await loginUserService.execute(email, password)
 
     expect(errors).toBe(null)
     expect(user).not.toBe(null)
@@ -27,7 +27,7 @@ describe('Login user service', () => {
     const email = 'johnpetros@mock.com'
     const password = 'jonhDOE77$'
 
-    const { errors, user } = await loginUser.execute(email, password)
+    const { errors, user } = await loginUserService.execute(email, password)
 
     expect(errors).toEqual(['usuário não encontrado'])
     expect(user).toBe(null)
@@ -37,7 +37,7 @@ describe('Login user service', () => {
     const email = 'johndoe@mock.com'
     const password = 'jonhDOE88$'
 
-    const { errors, user } = await loginUser.execute(email, password)
+    const { errors, user } = await loginUserService.execute(email, password)
 
     expect(errors).toEqual(['usuário não encontrado'])
     expect(user).toBe(null)
