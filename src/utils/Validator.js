e9dc51b47b183ddd19a883deb3dc610ea2b35257
João Pedro <joaopedro.nc@outlook.com>
2023-08-22 15:28:12 -0300

    🔒 Add user edition validation

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -3,29 +3,21 @@ const yup = require('yup')
 const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g
 
-const loginValidation = yup.object().shape({
-  email: yup
-    .string()
-    .required('Por favor, insira um e-mail')
-    .email('Por favor, insira um e-mail válido'),
-  password: yup
-    .string()
-    .required('Por favor, insira uma senha')
-    .matches(
-      passwordRegex,
-      'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial'
-    ),
-})
-
-const registerValidation = yup.object().shape({
+const nameValidation = yup.object().shape({
   name: yup
     .string()
     .required('Por favor, insira um nome de usuário')
     .min(3, 'Nome de usuário deve ter pelo menos 3 caracteres'),
+})
+
+const emailValidation = yup.object().shape({
   email: yup
     .string()
     .required('Por favor, insira um e-mail')
     .email('Por favor, insira um e-mail válido'),
+})
+
+const passwordValidation = yup.object().shape({
   password: yup
     .string()
     .required('Por favor, insira uma senha')
@@ -37,8 +29,52 @@ const registerValidation = yup.object().shape({
     .string()
     .required('Por favor, confirme sua senha')
     .oneOf([yup.ref('password')], 'Senhas não conferem'),
+})
+
+const roleValidation = yup.object().shape({
   role_id: yup.string().required('Por favor, escolha uma função'),
 })
+
+const loginValidation = emailValidation.concat(
+  yup.object().shape({
+    password: passwordValidation.fields.password,
+  })
+)
+
+const registerValidation = nameValidation.concat(
+  emailValidation,
+  passwordValidation,
+  roleValidation
+)
+
+const userEditionValidation = nameValidation.concat(
+  emailValidation,
+  roleValidation,
+  yup.object().shape(
+    {
+      password: yup
+        .string()
+        .nullable()
+        .notRequired()
+        .when('password', {
+          is: (value) => value?.length,
+          then: passwordValidation.fields.password,
+        }),
+      password_confirmation: yup
+        .string()
+        .nullable()
+        .notRequired()
+        .when('password_confirmation', {
+          is: (value) => value?.length,
+          then: passwordValidation.fields.password_confirmation,
+        }),
+    },
+    [
+      ['password', 'password'],
+      ['password_confirmation', 'password_confirmation'],
+    ]
+  )
+)
 
 const orderValidation = yup.object().shape({
   title: yup.string().required('Uma solicitação deve ter um título'),
@@ -86,6 +122,24 @@ class Validator {
         title,
         description,
         patrimony_id,
+      })
+    )
+  }
+
+  async validateUserEdition({
+    name,
+    email,
+    password,
+    password_confirmation,
+    role_id,
+  }) {
+    return this.validate(() =>
+      userEditionValidation.validate({
+        name,
+        email,
+        password,
+        password_confirmation,
+        role_id,
       })
     )
   }
