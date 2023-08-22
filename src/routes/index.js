7851950170272916d5ddfcc7551fd9f68e6a76a9
João Pedro <joaopedro.nc@outlook.com>
2023-08-22 12:34:11 -0300

    🐛 fix: Resolve Users router key

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ const { Router } = require('express')
 
 const sessionRouter = require('./session.routes')
 const ordersRouter = require('./orders.routes')
+const usersRouter = require('./users.routes')
 
 const router = Router()
 
 router.use(sessionRouter)
 
 router.use(ordersRouter)
+
+router.use(usersRouter)
 
 // Route Not Found
 router.get('*', (_, response) =>
