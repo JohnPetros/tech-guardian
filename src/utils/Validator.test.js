08c5f87259272b552c31944027993fcfe5fff782
João Pedro <joaopedro.nc@outlook.com>
2023-09-02 09:25:11 -0300

    🧪 Test Validator

--- src/utils/Validator.test.js
+++ src/utils/Validator.test.js
@@ -17,8 +17,8 @@ describe('Validator', () => {
     )
 
     expect(errors).toEqual([
-      'Por favor, insira um e-mail válido',
       'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
+      'Por favor, insira um e-mail válido',
     ])
   })
 
@@ -28,11 +28,13 @@ describe('Validator', () => {
 
     const errors = await validator.validateLogin(emptyEmail, emptyPassword)
 
-    expect(errors).toEqual([
-      'Por favor, insira um e-mail',
-      'Por favor, insira uma senha',
-      'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
-    ])
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        'Por favor, insira um e-mail',
+        'Por favor, insira uma senha',
+        'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
+      ])
+    )
   })
 
   it('should validate correct login input', async () => {
@@ -69,34 +71,92 @@ describe('Validator', () => {
 
     const errors = await validator.validateRegister(correctInput)
 
-    expect(errors).toEqual([
-      'Por favor, insira um nome de usuário',
-      'Nome de usuário deve ter pelo menos 3 caracteres',
-      'Por favor, insira um e-mail',
-      'Por favor, confirme sua senha',
-      'Por favor, insira uma senha',
-      'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
-      'Por favor, escolha uma função',
-    ])
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        'Por favor, insira um e-mail',
+        'Por favor, insira um nome de usuário',
+        'Nome de usuário deve ter pelo menos 3 caracteres',
+        'Por favor, confirme sua senha',
+        'Por favor, insira uma senha',
+        'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
+        'Por favor, escolha uma função',
+      ])
+    )
   })
 
   it('should validate incorrect register input', async () => {
-    const correctInput = {
+    const incorrectInput = {
       name: 'jo',
       email: 'johnpetros.com',
-      password: '777',
-      passwordConfirmation: 'johnPETROS777$',
+      password: 'jonasssss',
+      passwordConfirmation: 'não sei',
       roleId: '555',
     }
 
-    const errors = await validator.validateRegister(correctInput)
+    const errors = await validator.validateRegister(incorrectInput)
 
-    expect(errors).toEqual([
-      'Nome de usuário deve ter pelo menos 3 caracteres',
-      'Por favor, insira um e-mail válido',
-      'Por favor, confirme sua senha',
-      'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
-      'Por favor, escolha uma função',
-    ])
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        'Por favor, insira um e-mail válido',
+        'Por favor, confirme sua senha',
+        'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
+        'Por favor, escolha uma função',
+        'Nome de usuário deve ter pelo menos 3 caracteres',
+      ])
+    )
+  })
+
+  it('should not validate passwords when not provided', async () => {
+    const user = {
+      name: 'John Petros',
+      email: 'johnpetros@gmial.com',
+      password: 'john',
+    }
+
+    const errors = await validator.validateRegister(user)
+
+    expect(errors).toEqual(
+      expect.not.arrayContaining([
+        'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
+        'Senhas não conferem',
+      ])
+    )
+  })
+
+  it('should validate passwords when provided', async () => {
+    const user = {
+      name: 'John Petros',
+      email: 'johnpetros@gmial.com',
+      password: 'john',
+      password_confirmation: 'johnPETROS',
+      role_id: 'bc1cebbc-f20e-409d-ad7f-f567490c0953',
+    }
+
+    const errors = await validator.validateRegister(user)
+
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        'Sua senha deve conter pelo menos uma letra minúscula, uma maiúscula, um dígito e um caractere especial',
+        'Senhas não conferem',
+      ])
+    )
+  })
+
+  it('should validate incorrect order', async () => {
+    const order = {
+      title: '',
+      description: '',
+      patrimony_id: '',
+    }
+
+    const errors = await validator.validateOrder(order)
+
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        'Uma solicitação deve conter um título',
+        'Uma solicitação deve conter uma breve descrição do problema',
+        'Uma solicitção deve estar associado a um patrimônio',
+      ])
+    )
   })
 })
