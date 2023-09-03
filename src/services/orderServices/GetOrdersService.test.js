36460ddf589304ad579106a751b0927831ecf884
João Pedro <joaopedro.nc@outlook.com>
2023-09-03 20:55:51 -0300

    🧪 Test Get orders by date

--- src/services/orderServices/GetOrdersService.test.js
+++ src/services/orderServices/GetOrdersService.test.js
@@ -11,7 +11,7 @@ describe('Get orders service', () => {
   })
 
   it('should be able to get all open orders', async () => {
-    const orders = await getOrdersService.execute({ isOpen: true })
+    const { orders } = await getOrdersService.execute({ isOpen: true })
 
     orders.forEach((order) => {
       expect(order.is_open).toBe(true)
@@ -19,7 +19,7 @@ describe('Get orders service', () => {
   })
 
   it('should be able to get order by search', async () => {
-    const orders = await getOrdersService.execute({
+    const { orders } = await getOrdersService.execute({
       isOpen: true,
       search: 'Máquina de lavar',
     })
@@ -30,7 +30,7 @@ describe('Get orders service', () => {
   })
 
   it('should be able to get order by patrimonies_ids', async () => {
-    const orders = await getOrdersService.execute({
+    const { orders } = await getOrdersService.execute({
       isOpen: false,
       patrimonies_ids: ['b2737076-dcfe-4d8f-b4b3-76dcafdd99ef'],
     })
@@ -38,6 +38,16 @@ describe('Get orders service', () => {
     orders.forEach((order) => {
       expect(order.is_open).toBe(false)
       expect(order.patrimony_id).toBe('b2737076-dcfe-4d8f-b4b3-76dcafdd99ef')
+    })
+  })
+
+  it('should be able to get order by date', async () => {
+    const { orders } = await getOrdersService.execute({
+      date: '2023-08-20',
+    })
+
+    orders.forEach((order) => {
+      expect(order.created_at.includes('20/08/23')).toBe(true)
     })
   })
 })
