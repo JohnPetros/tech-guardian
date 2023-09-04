47d5569c40963806aa6e0894cb5b08e518e4d7ff
João Pedro <joaopedro.nc@outlook.com>
2023-09-04 15:55:21 -0300

    ✨ feat: Edit patrimony

---
+++ src/services/patrimonyServices/EditPatrimonyService.js
@@ -0,0 +1,31 @@
+const Validator = require('../../utils/Validator')
+const uuid = require('uuid')
+
+class EditPatrimonyService {
+  constructor(patrimonyModel) {
+    this.patrimonyModel = patrimonyModel
+  }
+
+  async execute(id, number) {
+    if (!uuid.validate(id)) {
+      return ['Patrimônio não encontrado']
+    }
+
+    const validator = new Validator()
+
+    const errors = await validator.validatePatrimony(number)
+
+    if (errors) return errors
+
+    const patrimony = await this.patrimonyModel.getById(id)
+
+    if (!patrimony) return ['Patrimônio não encontrado']
+
+    if (patrimony.number === number)
+      return ['Número de patrimônio já existente']
+
+    await this.patrimonyModel.edit(id, number)
+  }
+}
+
+module.exports = EditPatrimonyService
