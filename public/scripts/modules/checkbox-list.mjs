eebfaf9d602d476e3762221d4eaf1e1c62af82a7
João Pedro <joaopedro.nc@outlook.com>
2023-09-04 10:08:13 -0300

    ♿ Check checkbox on enter key

--- public/scripts/modules/checkbox-list.mjs
+++ public/scripts/modules/checkbox-list.mjs
@@ -12,16 +12,46 @@ export class CheckboxList {
     }
   }
 
-  closeAll() {
+  close() {
     this.checkboxLists.forEach((checkboxList) => {
       const checkboxes = checkboxList.querySelector('.checkboxes')
       checkboxes.classList.remove('open')
+
+      const checks = checkboxes.querySelectorAll('.checkbox')
+      checks.forEach((check) =>
+        check.removeEventListener('keydown', this.onCheckKeydown)
+      )
+    })
+  }
+
+  open() {
+    this.checkboxLists.forEach((checkboxList) => {
+      const checkboxes = checkboxList.querySelector('.checkboxes')
+      checkboxes.classList.add('open')
+
+      const checks = checkboxes.querySelectorAll('.checkbox')
+      checks.forEach((check) =>
+        check.addEventListener('keydown', this.onCheckKeydown)
+      )
     })
   }
 
   onCheckboxlistButtonClick({ currentTarget }) {
     const checkboxList = currentTarget.parentElement
     const checkboxes = checkboxList.querySelector('.checkboxes')
-    checkboxes.classList.toggle('open')
+
+    const isOpen = checkboxes.classList.contains('open')
+
+    if (isOpen) {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+  onCheckKeydown({ key, currentTarget }) {
+    if (key === 'Enter') {
+      currentTarget.click()
+    }
   }
 }
