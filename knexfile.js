15a0b6f31dfc6304c701d51d3b519a9a98cd1e95
João Pedro <joaopedro.nc@outlook.com>
2023-09-28 17:35:17 -0300

    🔧 chore: update knex file config

--- knexfile.js
+++ knexfile.js
@@ -5,22 +5,20 @@ const path = require('path')
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: process.env.DATABASE_CLIENT,
-    connection: process.env.DATABASE_URL,
+  client: process.env.DATABASE_CLIENT,
+  connection: process.env.DATABASE_URL,
 
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-    },
-    seeds: {
-      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-    },
-    useNullAdsDefault: true,
-    pool: {
-      max: 3,
-      min: 0,
-      idleTimeoutMillis: 1000,
-    },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+  },
+  useNullAdsDefault: true,
+  pool: {
+    max: 3,
+    min: 0,
+    idleTimeoutMillis: 1000,
   },
 }
--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const environment = process.env.ENVIRONMENT;
 const knexConfig = require("../../knexfile");
-const knex = require("knex")(knexConfig[environment])
+const knex = require("knex")(knexConfig)
 
 module.exports = knex
