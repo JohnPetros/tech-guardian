3873b410d7038dc30686737aa17c2864f23d98bb
João Pedro <joaopedro.nc@outlook.com>
2023-09-28 17:36:26 -0300

    🔧 chore: add node polifies to webpack config

--- webpack.server.js
+++ webpack.server.js
@@ -2,6 +2,16 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+    },
+  },
   entry: './src/server.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -17,4 +27,5 @@ module.exports = {
     ],
   },
   externals: [nodeExternals()],
+  plugins: [new NodePolyfillPlugin()],
 }
