752f4a7a308638c3614c287d3494a9b8403067c9
João Pedro <joaopedro.nc@outlook.com>
2023-09-29 12:02:54 -0300

    🔧 chore: add env config to knex

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,41 @@
 require('dotenv').config()
 const path = require('path')
 
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: process.env.DATABASE_CLIENT,
-  connection: process.env.DATABASE_URL,
+  development: {
+    client: process.env.DATABASE_CLIENT,
+    connection: process.env.DATABASE_URL,
 
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    },
+    useNullAdsDefault: true,
+    pool: {
+      max: 3,
+      min: 0,
+      idleTimeoutMillis: 1000,
+    },
   },
-  seeds: {
-    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-  },
-  useNullAdsDefault: true,
-  pool: {
-    max: 3,
-    min: 0,
-    idleTimeoutMillis: 1000,
+  production: {
+    client: process.env.DATABASE_CLIENT,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    pool: {
+      max: 3,
+      min: 0,
+      idleTimeoutMillis: 1000,
+    },
+    useNullAsDefault: true,
   },
 }
--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const environment = process.env.ENVIRONMENT;
-const knexConfig = require("../../knexfile");
-const knex = require("knex")(knexConfig)
+const environment = process.env.ENVIRONMENT
+const knexConfig = require('../../knexfile')
+const knex = require('knex')(knexConfig[environment])
 
 module.exports = knex
